7397e7077ee0ff0f2ec5d53c9067d2effd5846e3
ohnosisyphus <ohnosisyphus@gmail.com>
2021-06-09 18:27:34 +0800

    Better Version

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 password = 'a123456'
 remain_chance = 3 #剩餘機會
-while True:
+while remain_chance > 0:
+	remain_chance = remain_chance - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功!')
 		break #逃出迴圈
 	else:
-		remain_chance = remain_chance - 1
-		print('密碼錯誤! 還有', remain_chance, '次機會')
-		if remain_chance == 0:
-			print('密碼錯誤! 程序結束')
-			break
+		print('密碼錯誤!')
+		if remain_chance > 0:
+			print('還有', remain_chance, '次機會')
+		else:
+			print('沒機會了，程式結束')		
